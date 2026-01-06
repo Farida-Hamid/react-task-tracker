d61e68a0d1d591bac81629f52bdcd5a55fc68ebe
Farida-Hamid <faridaaddala@gmail.com>
2026-01-06 15:44:54 +0300

    Adjust the App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './Header'
+import TaskList from './TaskList'
+import TaskInput from './Task Input'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [tasks, setTasks] = useState([])
+  
+  const addTask = (newText) => {
+    setTasks([...tasks, newText])
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <main>
+        <Header count={tasks.length} />
+        <TaskInput onAdd={addTask} />
+        <TaskList item={tasks} />
+    </main>
   )
 }
 
